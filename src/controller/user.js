8e2ed2b90e274ebe9ebb1a731e2180450882096b
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-06-20 13:15:31 +0100

    feat: use login API

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,7 +4,9 @@
  */
 const { getUserInfo, createUser } = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
-const { registerUserNameNotExistInfo, registerUserNameExistInfo } = require('../model/ErrorInfo')
+const { registerUserNameNotExistInfo, 
+        registerUserNameExistInfo,
+        loginFailInfo } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 /**
  * 用户名是否存在
@@ -48,7 +50,30 @@ async function register({userName, password, gender}) {
     }
 }
 
+/**
+ * login
+ * @param {Object} ctx koa2 ctx
+ * @param {string} userName 
+ * @param {string} password 
+ */
+async function login(ctx, userName, password) {
+    //登录成功后 ctx.session.userInfo = xxx(session存入用户信息)
+    const userInfo = await getUserInfo(userName, doCrypto(password))
+    if (!userInfo) {
+        //登陆失败
+        return new ErrorModel(loginFailInfo)
+    }
+
+    //登陆成功
+    if (ctx.session.userInfo == null) {
+        ctx.session.userInfo = userInfo
+    }
+
+    return new SuccessModel()
+}
+
 module.exports = {
     isExist,
-    register
+    register,
+    login
 }
