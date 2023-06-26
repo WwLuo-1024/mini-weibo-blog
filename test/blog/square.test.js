1f589ba1f8251efae274db0e5e57a72b98e0d08b
WwLuo-1024 <abrahamwong1408@gmail.com>
2023-06-26 23:36:42 +0100

    test: unit test of square

---
+++ test/blog/square.test.js
@@ -0,0 +1,21 @@
+/**
+ * @description 
+ * @author Luo Wang
+ */
+
+const server = require('../server')
+const { COOKIE } = require('../testUserInfo')
+
+//加载第一页数据
+test('Square, Loading first page data', async () => {
+    const res = await server
+            .get(`/api/square/loadMore/0`)
+            .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
+})
